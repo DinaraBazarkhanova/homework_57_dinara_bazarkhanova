d38f6d992ed95da7f3c0a0538b9b91745640dd84
Dinara Bazarkhanova <dikosha8.00@gmail.com>
2023-03-23 17:56:20 +0100

    Изминение в файле base.py

--- source/webapp/views/base.py
+++ source/webapp/views/base.py
@@ -1,14 +1,25 @@
-from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from django.views import View
 
 from webapp.forms import ArticleForm
 from webapp.models import Article
 
 
-def index_view(request: WSGIRequest):
-    articles = Article.objects.exclude(is_deleted=True)
-    context = {
-        'articles': articles,
-        'form': ArticleForm()
-    }
-    return render(request, 'index.html', context=context)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.exclude(is_deleted=True)
+        context = {
+            'articles': articles,
+            'form': ArticleForm()
+        }
+        return render(request, 'index.html', context=context)
+
+
+# class IndexView(View):
+#     def get(self, request, *args, **kwargs):
+#         articles = Article.objects.exclude(is_deleted=True)
+#         context = {
+#             'articles': articles,
+#             'form': ArticleForm()
+#         }
+#         return render(request, 'index.html', context=context)
